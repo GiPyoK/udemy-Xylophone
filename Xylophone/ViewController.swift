a8ef26a7594f2253222a6fd6da4e2677dba0399e
Gi Pyo Kim <gipyok@gmail.com>
2021-09-13 22:41:29 -0700

    Implemented playSound function to play the notes from the library. Applied dimming animation to the note being pressed.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,17 +7,43 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
     @IBAction func keyPressed(_ sender: UIButton) {
+        
+        // Play sound
+        playSound(key: sender.currentTitle ?? "C")
+        
+        // Dim the button
+        DispatchQueue.main.async {
+            UIView.animate(withDuration: 0.2) {
+                sender.alpha = 0.5
+            }
+            
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            UIView.animate(withDuration: 0.2) {
+                sender.alpha = 1.0
+            }
+        }
     }
+    
+    func playSound(key: String) {
+            let url = Bundle.main.url(forResource: key, withExtension: "wav")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+                    
+        }
+
     
 }
 
